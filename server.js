09dbc6d8b53ea281169cc7f116ab78edd0ebba0c
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-02-25 11:32:43 +0900

    sqlを使えるようにした

--- server.js
+++ server.js
@@ -3,24 +3,28 @@ import expressWs from "express-ws";
 const wsInstance = expressWs(express());
 const app = wsInstance.app;
 
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient();
+
 app.use(express.static("dist"));
 
-const messages = [];
-
 app.ws("/", function (ws, req) {
-  const broadcast = () => {
+  const broadcast = async () => {
+    const messages = await client.message.findMany();
     wsInstance.getWss("/").clients.forEach((c) => {
-      c.send(JSON.stringify(messages));
+      c.send(JSON.stringify(messages.map((m) => (m.text))));
     });
   };
-  ws.on("message", function (msg) {
+  ws.on("message", async function (msg) {
     console.log(msg);
     const json = JSON.parse(msg);
     if (json.type === "message") {
-      messages.push(json.data);
-      broadcast();
+      // messages.push(json.data);
+      await client.message.create({ data: { name: "test", text: json.data } });
+      await broadcast();
     } else if (json.type === "fetch") {
-      ws.send(JSON.stringify(messages));
+      const messages = await client.message.findMany();
+      ws.send(JSON.stringify(messages.map((m) => (m.text))));
     }
   });
   // console.log('socket', req.testing);
