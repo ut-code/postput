2f5b8714160b163ca484e8fb3a359d6bb9418086
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-03-04 16:49:28 +0900

    中央列のレイアウトを編集した

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 import { useSocket } from "./socket";
 
 import Grid from "@mui/material/Grid";
+import Stack from "@mui/material/Stack";
+import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
@@ -13,47 +15,75 @@ function App() {
   const { send, messages } = useSocket();
 
   const [text, setText] = useState("");
+  const headerHeight = 60;
+  const footerHeight = 60;
   return (
     <>
-      useStateのテスト:
-      <Button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </Button>
-      <Grid container spacing={1} alignItems="center">
-        <Grid item xs={12}>
-          メッセージ一覧:
-        </Grid>
-        {messages.map((m) => (
-          <>
-            <Grid item xs={12} key={m.id}>
+      <Box
+        sx={{
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: headerHeight,
+        }}
+      >
+        メッセージ一覧:
+      </Box>
+      <Box sx={{ overflow: "auto", height: "100%" }}>
+        <Box sx={{ height: headerHeight }} />
+        <Stack spacing={1}>
+          {messages.map((m) => (
+            <Box key={m.id} sx={{ border: 1 }}>
+              {m.name +
+                " " +
+                new Date(m.sendTime).toDateString() +
+                " " +
+                new Date(m.sendTime).toTimeString() +
+                " " +
+                JSON.stringify(m.tags)}{" "}
+              <br />
               {m.text}
-            </Grid>
-          </>
-        ))}
-        <Grid item xs>
-          <TextField
-            fullWidth
-            variant="standard"
-            value={text}
-            onChange={(e) => {
-              setText(e.target.value);
-            }}
-            label="送信するテキスト"
-          />
+            </Box>
+          ))}
+        </Stack>
+        <Box sx={{ height: footerHeight }} />
+      </Box>
+      <Box
+        sx={{
+          position: "absolute",
+          left: 0,
+          bottom: 0,
+          width: "100%",
+          height: footerHeight,
+        }}
+      >
+        <Grid container spacing={1} alignItems="center">
+          <Grid item xs>
+            <TextField
+              fullWidth
+              variant="standard"
+              value={text}
+              onChange={(e) => {
+                setText(e.target.value);
+              }}
+              label="送信するテキスト"
+            />
+          </Grid>
+          <Grid item>
+            <Button
+              variant="contained"
+              onClick={() => {
+                send({ text: text });
+                setText("");
+              }}
+              endIcon={<SendIcon />}
+            >
+              送信
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item>
-          <Button
-            variant="contained"
-            onClick={() => {
-              send({text: text});
-              setText("");
-            }}
-            endIcon={<SendIcon />}
-          >
-            送信
-          </Button>
-        </Grid>
-      </Grid>
+      </Box>
     </>
   );
 }
